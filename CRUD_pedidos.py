cef2a19d0c70650005822b794f89cce1be63d5f6
Elis <elismaidi@gmail.com>
2025-05-21 17:10:49 -0300

    feat: adiciona função criar_pedido

--- CRUD_pedidos.py
+++ CRUD_pedidos.py
@@ -1,2 +1,105 @@
+import pandas as pd
+import os
+from CRUD_cardapio import listar_pratos
+
+arquivoPedido = "pedidos.json"
+arquivoCardapio = "cardapio.json"
+
+def criar_pedido():
+
+    print("\n--- Criar novo pedido ---")
+    print("\n--- Cardápio do Restaurante ---")
+   # listar_pratos()
+
+    lNomes = []
+    lQuantidade = []
+
+    
+    while(True):
+
+        if (len(lNomes)== 0): 
+            nome = input("Insira o nome do prato: ")
+            
+        else: 
+            opcao = input("Deseja inserir outro prato? ('Sim' / 'Não'): ")
+            
+            if opcao == "Sim" or opcao == "sim" or opcao == "SIM":
+                nome = input("Insira o nome do prato: ")
+            else:
+                break
+
+
+        quantidade = int(input("Insira a quantidade: "))
+
+        if nome in lNomes:
+            indexNome = lNomes.index(nome)
+            lQuantidade[indexNome] += quantidade
+        else:
+            lNomes.append(nome)
+            lQuantidade.append(quantidade)
+
+
+    pedido = {
+        "pratos" : [lNomes],
+        "quantidades" : [lQuantidade]
+    }
+
+    df = pd.DataFrame(pedido)
+
+    # checando se o arquivo existe
+    if os.path.exists(arquivoPedido):
+
+        # checando se tem algo dentro do arquivo
+        if os.path.getsize(arquivoPedido) > 0:
+
+            # se tiver, ele pega o(s) prato(s) existente(s) e concatena com o novo prato
+            df_antigo = pd.read_json(arquivoPedido)
+            df_final = pd.concat([df_antigo, df])
+
+        # se não tiver nada dentro do arquivo, ele coloca o novo prato como primeiro item
+        else:
+            df_final = df
+
+        # converte o cardapio atualizado para json    
+        df_final.to_json(arquivoPedido, indent=4, orient='records')
+
+    # se o arquivo não existir, ele cria um e coloca o novo prato dentro   
+    else:
+        df.to_json(arquivoPedido, indent=4, orient='records')
+
+
+def listar_pedidos():
+    ...
+
+
+def atualizar_pedido():
+    ...
+
+def deletar_pedido():
+    ...
+
+
+
 def pedidos():
-    print("\nPedidos veyr\n")
+
+    print("\n1- Criar pedido\n2- Listar pedidos\n3- Atualizar pedido\n4- Deletar pedido\n5- Voltar ao menu principal")
+    opcao = int(input("Escolha sua opção: "))
+
+    if opcao == 1:
+        criar_pedido()
+
+    elif opcao == 2:
+        listar_pedidos()
+
+    elif opcao == 3:
+        atualizar_pedido()
+
+    elif opcao == 4:
+        deletar_pedido()
+
+    elif opcao == 5:
+        return
+    
+    else:
+        print("\nOpção inválida, selecione uma opção válida!\n")
+        pedidos()
